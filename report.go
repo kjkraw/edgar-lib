fffd6095ce78e78039e4df641cc2062d2ec809ae
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2023-01-11 14:30:51 -0800

    Add GetReport, a function which gets the XBRL instance file for a given filing.

---
+++ report.go
@@ -0,0 +1,63 @@
+package edgar
+
+import (
+	"fmt"
+	"github.com/beevik/etree"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// getInstanceUrl returns the URI of the XBRL Instance file for a given filing.
+// The function requests and processes a filing summary to find the name of the instance.
+// If the function returns a RequestError with NotFound status, this may mean that the filing does not contain XBRL.
+func getInstanceUrl(cik int, accessionNumber string, client *Client) (url string, err error) {
+	baseUrl := fmt.Sprintf("https://www.sec.gov/Archives/edgar/data/%010d/%s/", cik, strings.Replace(accessionNumber, "-", "", 2))
+
+	res, err := client.Get(baseUrl + "FilingSummary.xml")
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		err = &RequestError{StatusCode: res.StatusCode}
+		return
+	}
+
+	filingSum := etree.NewDocument()
+	if _, err = filingSum.ReadFrom(res.Body); err != nil {
+		return
+	}
+
+	instance := filingSum.Root().SelectElement("MyReports").SelectElements("Report")[0].SelectAttr("instance").Value
+	if strings.HasSuffix(instance, ".htm") {
+		instance = fmt.Sprintf("%s_htm.xml", strings.TrimSuffix(instance, ".htm"))
+	}
+	url = baseUrl + instance
+	return
+}
+
+// GetReport copies the XBRL instance file for a given filing into w.
+// The content of w can then be processed using the xbrl package.
+// This function cannot download reports that do not have an XBRL instance file.
+func GetReport(w io.Writer, cik int, accessionNumber string, client *Client) (err error) {
+	reportUrl, err := getInstanceUrl(cik, accessionNumber, client)
+	if err != nil {
+		return
+	}
+
+	res, err := client.Get(reportUrl)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		err = &RequestError{StatusCode: res.StatusCode}
+		return
+	}
+
+	_, err = io.Copy(w, res.Body)
+	return
+}
